1cef51d5fb6934bd1ea0949b31ac233ac2c1229a
Jojo <phantasmasage@outlook.com>
2021-09-30 23:18:38 +0000

    final commit

--- SayHi.java
+++ SayHi.java
@@ -3,8 +3,13 @@ public class SayHi{
    for(int i=0;i<5;i++){
     System.out.println("Hello world");
      }
+  
      int a = 5, b = 3, c = 0;
      c = a + b;
      System.out.println("5 + 3 = " + c); 
+     if(c >=5){
+      System.out.println("java is fun");
+      }
+  
+   } 
   }
- }  
